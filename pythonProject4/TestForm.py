c4cb3539827f2187c56687cecac9a5ae9ec89b80
SetMyGod1488 <ivangritsina@gmail.com>
2022-04-05 21:13:26 +0300

    Автотесты

--- pythonProject4/TestForm.py
+++ pythonProject4/TestForm.py
@@ -1,29 +1,38 @@
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+
+import math
 import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://suninjuly.github.io/registration1.html"
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/math.html"
 browser = webdriver.Chrome()
 browser.get(link)
-input1 = browser.find_element(By.CSS_SELECTOR, 'body > div > form > div.first_block > div.form-group.first_class > input')
-input1.send_keys('Ivan')
 
-input2 = browser.find_element(By.CSS_SELECTOR, 'body > div > form > div.first_block > div.form-group.second_class > input')
-input2.send_keys('Sokolov')
+x_element = browser.find_element(By.ID, 'input_value')
+x = x_element.text
+y = calc(x)
 
-input3 = browser.find_element(By.CSS_SELECTOR, 'body > div > form > div.first_block > div.form-group.third_class > input')
-input3.send_keys('gis20072017@gmail.com')
+field = browser.find_element(By.ID, 'answer')
+field.send_keys(calc(x))
 
-time.sleep(5)
+checkbox = browser.find_element(By.ID, 'robotCheckbox')
+checkbox.click()
 
-button = browser.find_element(By.CSS_SELECTOR, 'button')
+radio = browser.find_element(By.ID, 'robotsRule')
+radio.click()
+
+button = browser.find_element(By.TAG_NAME, 'button')
 button.click()
-time.sleep(5)
 
-welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-welcome_text = welcome_text_elt.text
-
-assert "Congratulations! You have successfully registered!" == welcome_text
 time.sleep(10)
-browser.quit()
 
+browser.quit()
